d2d0fa23ca8ebcbf1b35a82a31365af278c75942
Krzysztof Sakrejda-Leavitt <sakrejda@cns.umass.edu>
2014-03-30 18:13:41 -0400

    I'm ready to start using Rcpp modules. Gah.

---
+++ R/time_series-handle.R
@@ -0,0 +1,266 @@
+time_series_handle <- setRefClass(Class = "time_series_handle",
+    fields = list(
+        y_ = "numeric",
+        mins_ = "numeric",
+        maxs_ = "numeric",
+        x_ = "numeric",
+        drift_ = "numeric",
+        tails_ = "numeric",
+        scales_ = "numeric",
+        obs_scales_ = "numeric",
+        distr_ = "character",
+        distribution_type = "list",
+        time_series = function(x=NULL) {
+            if (is.null(x)) {
+                locs_ <<- c(.Call(
+                    "get_map", xp=manage_these_ptrs[['data_ptr']], s="observed", PACKAGE="Rux"))
+                return(locs_)
+            } else if (length(x) == length(locs_)) {
+                locs_ <<- c(.Call(
+                    "set_map", xp=manage_these_ptrs[['data_ptr']], s="observed", x=x, PACKAGE="Rux"))
+                return(locs_)
+            }
+        },
+        minima = function(x=NULL) {
+            if (is.null(x)) {
+                mins_ <<- c(.Call(
+                    "get_map", xp=manage_these_ptrs[['data_ptr']], s="minima", PACKAGE="Rux"))
+                return(mins_)
+            } else if (length(x) == length(mins_)) {
+                mins_ <<- c(.Call(
+                    "set_map", xp=manage_these_ptrs[['data_ptr']], s="minima", x=x, PACKAGE="Rux"))
+                return(mins_)
+            }
+        },
+        maxima = function(x=NULL) {
+            if (is.null(x)) {
+                maxs_ <<- c(.Call(
+                    "get_map", xp=manage_these_ptrs[['data_ptr']], s="maxima", PACKAGE="Rux"))
+                return(maxs_)
+            } else if (length(x) == length(maxs_)) {
+                maxs_ <<- c(.Call(
+                    "set_map", xp=manage_these_ptrs[['data_ptr']], s="maxima", x=x, PACKAGE="Rux"))
+                return(maxs_)
+            }
+        },
+        draws = function(x=NULL) {
+            if (is.null(x)) {
+                .self$draw()
+                draws_ <<- c(.Call("get_map", xp=manage_these_ptrs[['theta_ptr']], s="draws", PACKAGE="Rux"))
+                return(draws_)
+            } else if (length(x) == length(draws_)) {
+                draws_ <<- c(.Call("set_map", xp=manage_these_ptrs[['theta_ptr']], s="draws", x=x, PACKAGE="Rux"))
+                return(draws_)
+            }
+        },
+        observation_scales = function(x=NULL) {
+            if (is.null(x)) {
+                obs_scales_ <<- c(.Call("get_map", xp=manage_these_ptrs[['theta_ptr']], s="obs_scales", PACKAGE="Rux"))
+                return(obs_scales_)
+            } else if (length(x) == length(obs_scales_)) {
+                obs_scales_ <<- c(.Call("set_map", xp=manage_these_ptrs[['theta_ptr']], s="obs_scales", x=x, PACKAGE="Rux"))
+                return(obs_scales_)
+            }
+        },
+        drift = function(x=NULL) {
+            if (is.null(x)) {
+                drift_ <<- c(.Call("get_map", xp=manage_these_ptrs[['theta_ptr']], s="drift", PACKAGE="Rux"))
+                return(drift_)
+            } else if (length(x) == length(drift_)) {
+                drift_ <<- c(.Call("set_map", xp=manage_these_ptrs[['theta_ptr']], s="drift", x=x, PACKAGE="Rux"))
+                return(drift_)
+            }
+        },
+        tails = function(x=NULL) {
+            if (is.null(x)) {
+                tails_ <<- c(.Call("get_map", xp=manage_these_ptrs[['theta_ptr']], s="tails", PACKAGE="Rux"))
+                return(tails_)
+            } else if (length(x) == length(tails_)) {
+                tails_ <<- c(.Call("set_map", xp=manage_these_ptrs[['theta_ptr']], s="tails", x=x, PACKAGE="Rux"))
+                return(tails_)
+            }
+        },
+        scales = function(x=NULL) {
+            if (is.null(x)) {
+                scales_ <<- c(.Call("get_map", xp=manage_these_ptrs[['theta_ptr']], s="scales", PACKAGE="Rux"))
+                return(scales_)
+            } else if (length(x) == length(scales_)) {
+                scales_ <<- c(.Call("set_map", xp=manage_these_ptrs[['theta_ptr']], s="scales", x=x, PACKAGE="Rux"))
+                return(scales_)
+            }
+        },
+        manage_these_ptrs = "list",
+        time_series_ptr = "externalptr",
+        rng_ptr = "externalptr"
+    ),
+    methods = list(
+        initialize = function(
+            y_at_times = NA,
+            minima_at_times = rep(NA, length(y_at_times)),
+            maxima_at_times = rep(NA, length(y_at_times)),
+            x_at_times = NULL,
+            drift = rep(0, length(y_at_times)),
+            tails = rep(NA, length(y_at_times)),
+            scales = rep(NA, length(y_at_times)),
+            obs_scales = rep(NA, length(y_at_times)),
+            distributions = NULL,
+            RNG=NULL, seed=sample(x=1:10^3, size=1)
+        ) {
+            # Checks:
+            competingData <- !is.na(y_at_times) & (!is.na(minima_at_times) | !is.na(maxima_at_times))
+            if (any(competingData)) {
+                whichCompetingData <- which(competingData)
+                msg <- paste(
+                    "Multiple data types specified for certain time points.  ",
+                    "Problems for: \n",
+                    paste("\t", whichCompetingData, "\n", sep='', collapse=""))
+                stop(msg)
+            }
+
+            if (is.null(x_at_times)) {
+                warning("Using observed values as initialis.  This might not work.")
+                x_at_times <- y_at_times
+            }
+
+
+            # Distributions:
+            if (is.null(distributions)) {
+                distr_ <<- c(
+                    'constant', 'uniform', 'ordered_uniform', 'normal', 't_walk',
+                    't_walk_open', 't_walk_open_reverse',
+                    't_walk_observed_normal', 't_walk_observed_interval'
+                )
+            } else {
+                distr_ <<- distributions
+            }
+
+            # RNG:
+            if (is.null(RNG)) {
+                rng_ptr <<- .Call("get_RNG", seed_=seed, PACKAGE="Rux")
+            } else {
+                rng_ptr <<- RNG
+            }
+
+            # Tracking C++ level types...
+            distribution_type <<- list()
+
+            # Set vars.
+            y_ <<- y_at_times   ## Used to be locs_ (observed)
+            mins_ <<- minima_at_times
+            maxs_ <<- maxima_at_times
+            x_ <<- x_at_times   ## Used to be draws_ (state)
+            drift_ <<- drift
+            tails_ <<- tails
+            scales_ <<- scales
+            obs_scales_ <<- obs_scales
+
+
+            # Pass down to C++
+            manage_these_ptrs <<- .Call("locations_init",
+                locations_=locs_, drift_ = drift_,
+                tails_=tails_, scales_=scales_,
+                obs_scales_ = obs_scales_,
+                minima_=mins_, maxima_=maxs_, draws_ = draws_,
+                xp_rng=rng_ptr, PACKAGE="Rux")
+
+            locations_ptr <<- manage_these_ptrs[['sampler_ptr']]
+            # Initialize (this is no longer quite right, resolve?)
+            # Much more ambiguous now how to do by default, it will have to be
+            # part of initalization, maybe just a loop of distribution names
+            # to add... plus checks.
+            for ( i in which(!is.na(locations) &  is.na(obs_scales) ) ) add_distribution(type='constant', which=i)
+#           for ( i in which(!is.na(locations) & !is.na(obs_scales) ) ) add_distribution(type='normal', which=i)
+#           for ( i in which(!is.na(minima) & !is.na(maxima)) ) add_distribution(type='uniform', which=i)
+
+            # Return
+            return(.self)
+        },
+        lpdf = function(x) c(.Call("locations_lpdfs", xp=locations_ptr, X=x, PACKAGE="Rux")),
+        state = function() c(.Call("locations_state", xp=locations_ptr, PACKAGE="Rux")),
+        add_distribution = function(type=NULL, which=NULL, ...) {
+            if (is.null(type) || !(type %in% distr_)) {
+                msg <- paste(
+                    "The 'type' parameter must be one of:\n", paste(distr_,collapse=', '),
+                    "\n")
+                stop(msg)
+            }
+            if ((which < 1) || (which > length(locs_))) {
+                msg <- paste(
+                    "The 'which' parameter must be an integer in the range
+                    [1,",length(locs_), "].\n", sep='')
+                stop(msg)
+            }
+
+            switch(EXPR=type,
+                constant = {
+                    .Call("locations_bind_constant",    xp=locations_ptr,
+                        which=which-1, PACKAGE="Rux")
+                },
+                uniform = {
+                    .Call("locations_bind_uniform",     xp=locations_ptr,
+                        which=which-1, xp_rng=rng_ptr, PACKAGE="Rux")
+                },
+                ordered_uniform = {
+                    .Call("locations_bind_ordered_uniform",     xp=locations_ptr,
+                        which=which-1, xp_rng=rng_ptr, PACKAGE="Rux")
+                },
+                normal = {
+                    .Call("locations_bind_normal", xp=locations_ptr,
+                        which=which-1, xp_rng=rng_ptr, PACKAGE="Rux")
+                },
+                t_walk = {
+                    .Call("locations_bind_t_walk",      xp=locations_ptr,
+                        which=which-1, xp_rng=rng_ptr, PACKAGE="Rux")
+                },
+                t_walk_open = {
+                    .Call("locations_bind_t_walk_open",         xp=locations_ptr,
+                        which=which-1, xp_rng=rng_ptr, PACKAGE="Rux")
+                },
+                t_walk_open_reverse = {
+                    .Call("locations_bind_t_walk_open_reverse",         xp=locations_ptr,
+                        which=which-1, xp_rng=rng_ptr, PACKAGE="Rux")
+                },
+                t_walk_observed_normal = {
+                    .Call("locations_bind_t_walk_observed_normal", xp=locations_ptr,
+                        which=which-1, xp_rng=rng_ptr, PACKAGE="Rux")
+                },
+                t_walk_observed_interval = {
+                    .Call("locations_bind_t_walk_observed_interval", xp=locations_ptr,
+                        which=which-1, xp_rng=rng_ptr, PACKAGE="Rux")
+                }
+            )
+            distribution_type[[as.character(which)]] <<- type
+            return(0)
+        },
+        drop_distribution = function(which=NULL) {
+            if ((which < 1) || (which > length(locs_))) {
+                msg <- paste(
+                    "The 'which' parameter must be an integer in the range
+                    [1,",length(locs_), "].\n", sep='')
+                stop(msg)
+            } else {
+                .Call("locations_drop_distribution", xp=locations_ptr,
+                            which=which-1, PACKAGE="Rux")
+            }
+            return(0)
+        },
+        drop_all = function(which=NULL) {
+            for ( i in 1:length(locs_)) { drop_distribution(i) }
+        },
+        draw = function() {
+            .Call("locations_draw", xp=locations_ptr, PACKAGE="Rux")
+        }
+    )
+)
+
+#locations_handle$lock('locs_')
+#locations_handle$lock('drift_')
+#locations_handle$lock('tails_')
+#locations_handle$lock('scales_')
+#locations_handle$lock('obs_scales_')
+#locations_handle$lock('mins_')
+#locations_handle$lock('maxs_')
+#locations_handle$lock('distr_')
+#
+
+
